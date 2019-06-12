50411e0b386e041dd9629e24cd2e7ab12c5e59db
TheDynamo36 <raviraj7852@gmail.com>
2019-06-12 11:01:38 +0530

    Bug Fixes and Added fetch button to automatically fetch data of selected row in in the data table.

---
+++ src/timer/Timer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timer;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author Gaurav_Sharma
+ */
+public class Timer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Clock().setVisible(true);
+    }
+    
+}
